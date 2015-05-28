f82e51448f1eca646e916268e896c9d466c40a0e
Riccardo Cagnasso <riccardo@phascode.org>
2015-05-28 18:55:49 +0200

    Initial

---
+++ BetterGuards/Guard.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BahaTurret;
+using KSPAchievements;
+using UnityEngine;
+
+namespace BetterGuards
+{
+    class Guard : ManagerBase
+    {
+        [KSPField(guiActiveEditor = true, isPersistant = true, guiActive = true, guiName = "Scan Interval"),
+            UI_FloatRange(minValue = 1f, maxValue = 60f, stepIncrement = 1f, scene = UI_Scene.All)]
+        public float TargetScanInterval = 8;
+
+        [KSPField(guiActiveEditor = true, isPersistant = true, guiActive = true, guiName = "Guard Max Range"),
+            UI_FloatRange(minValue = 100f, maxValue = 8000f, stepIncrement = 100f, scene = UI_Scene.All)]
+        public float GuardMaxRange = 1500f;
+
+        [KSPField(guiActiveEditor = true, isPersistant = true, guiActive = true, guiName = "Guard Min Range"),
+            UI_FloatRange(minValue = 0f, maxValue = 8000f, stepIncrement = 100f, scene = UI_Scene.All)]
+        public float GuardMinRange = 0f;
+
+        [KSPField(guiActiveEditor = true, isPersistant = true, guiActive = true, guiName = "Guard: "),
+            UI_Toggle(disabledText = "Enabled", enabledText = "Disabled")]
+        public bool Enabled = false;
+
+        public override void OnStart(PartModule.StartState state)
+        {
+            base.OnStart(state);
+
+            part.force_activate();
+        }
+
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+
+            if (HighLogic.LoadedSceneIsFlight && vessel.IsControllable && Enabled)
+            {
+                Debug.Log("1 On Guard Update");
+            }
+        }
+
+        public override void OnFixedUpdate()
+        {
+            base.OnFixedUpdate();
+
+            if (Enabled)
+            {
+                Debug.Log("On Guard Fixed Update");
+                var targets = FindTargets();
+
+                Debug.Log(targets);
+            }
+        }
+
+        public bool TargetInRangeAndSight(Vessel v)
+        {
+            var sqrDistance = (transform.position - v.transform.position).sqrMagnitude;
+
+            return sqrDistance <= Math.Pow(GuardMaxRange, 2) && sqrDistance >= Math.Pow(GuardMinRange, 2);
+        }
+
+        public IEnumerable<Vessel> FindTargets()
+        {
+            Debug.Log("Finding Targets");
+            var targets = new List<Vessel>();
+
+            foreach (var v in FlightGlobals.Vessels)
+            {
+                if (v.loaded && TargetInRangeAndSight(v))
+                {
+                    targets.Add(v);
+                }
+            }
+
+            return targets;
+        }
+    }
+}
---
+++ BetterGuards/ManagerBase.cs
@@ -0,0 +1,66 @@
+﻿using BahaTurret;
+using UnityEngine;
+
+namespace BetterGuards
+{
+    public class ManagerBase : PartModule
+    {
+        public AudioSource AudioSource;
+        public AudioSource WarningAudioSource;
+
+        #region guibuttons
+        [KSPField(guiActiveEditor = true, isPersistant = true, guiActive = true, guiName = "Team: "),
+            UI_Toggle(disabledText = "A", enabledText = "B")]
+        public bool Team = false;
+        #endregion
+
+        public override void OnStart(PartModule.StartState state)
+        {
+            base.OnStart(state);
+
+            AudioSource = InitializeAudioSource();
+            WarningAudioSource = InitializeAudioSource();
+
+        }
+
+        #region actions
+        [KSPAction("Toggle Team")]
+        public void AgToggleTeam(KSPActionParam param)
+        {
+            PlaySound("click");
+            Team = !Team;
+
+            Debug.Log("Toggle Team: " + Team);
+        }
+        #endregion
+
+        #region audio
+        public AudioSource InitializeAudioSource()
+        {
+            var audioSource = gameObject.AddComponent<AudioSource>();
+            audioSource.minDistance = 500;
+            audioSource.maxDistance = 1000;
+            audioSource.dopplerLevel = 0;
+            audioSource.volume = Mathf.Sqrt(GameSettings.UI_VOLUME);
+
+            return audioSource;
+        }
+
+        public void PlaySound(string name, bool warning = false)
+        {
+            var clip = GameDatabase.Instance.GetAudioClip("BetterGuards/Sounds/" + name);
+
+            if (warning)
+            {
+                WarningAudioSource.PlayOneShot(clip);
+            }
+            else
+            {
+                AudioSource.PlayOneShot(clip);
+            }
+
+            Debug.Log("Sound play: " + name);
+        }
+        #endregion
+    }
+}
